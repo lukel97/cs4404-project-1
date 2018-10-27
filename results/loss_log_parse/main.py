be4d7492476210b92f235ddae268d65588735698
Martino Mansoldo <mansoldm@tcd.ie>
2018-10-27 22:14:50 +0100

    average cycle consistency losses

--- results/loss_log_parse/main.py
+++ results/loss_log_parse/main.py
@@ -5,12 +5,11 @@ lines = [line for line  in open(sys.stdin.readline().strip())]
 parens_matcher = r'\((.*?)\)'
 translate = { 'D_A' : 'W2SDiscLoss',
                 'G_A':'W2SGenLoss',
-                'cycle_A':'CycleConsistencySLoss',
                 'idt_A':'IdtS',
                 'D_B':'S2WDiscLoss',
                 'G_B':'S2WGenLoss',
-                'cycle_B':'CycleConsistencyWLoss',
-                'idt_B': 'IdtW'}
+                'idt_B': 'IdtW',  
+                'CCL':'CycleConsistencyLoss'}
 files = {}
 column_names = 'Epoch,Step,Value\n'
 for key in translate:
@@ -22,21 +21,29 @@ print_every = 20
 for i, line in enumerate(lines):
     if not (i % print_every):
         print(str(i) +' lines processed'.format(i))
+
     line_match = re.findall(parens_matcher, line).pop()
     matches = [s for s in line_match.split(' ') if s]
     
-    epoch, iteration = matches[1].strip()[:-1], matches[3][:-1] 
+    epoch, iteration = matches[1][:-1], matches[3][:-1]
+
     exclude_parens_match = '(' + line_match+')'
     matches_losses = line[len(exclude_parens_match):].strip().split(' ')
     parsed_losses = {}
     i = 1
     while i < len(matches_losses):
-        parsed_losses[matches_losses[i - 1][:-1]] =  (matches_losses[i].strip())
+        parsed_losses[matches_losses[i - 1][:-1]] =  float((matches_losses[i].strip()))
         i += 2
+    
+    # average the cycle consistency losses
+    parsed_losses['CCL'] = (parsed_losses['cycle_A'] + parsed_losses['cycle_B'])/2
+    del parsed_losses['cycle_A']
+    del parsed_losses['cycle_B']
+
 
     for key in translate.keys():
         f = files[key]
-        f.write(epoch+','+iteration+','+parsed_losses[key]+'\n')
+        f.write(epoch+',' + iteration + ','+  str(parsed_losses[key])+'\n')
 
 
 for key in translate.keys():
